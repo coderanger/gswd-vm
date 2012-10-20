e441d3735b9e31b0d05d88b9808ce0d51c761653
Noah Kantrowitz <noah@coderanger.net>
2012-10-20 15:14:55 -0700

    First pass on the GSWD development VM.

---
+++ site-cookbooks/gswd/recipes/database.rb
@@ -0,0 +1,13 @@
+include_recipe 'postgresql::server'
+
+# Rewrite this resource to use our template instead
+template "#{node[:postgresql][:dir]}/pg_hba.conf" do
+  cookbook 'gswd'
+end
+
+# Create database user
+postgresql_database_user 'vagrant' do
+  action :create
+  connection({:host => "127.0.0.1", :port => 5432, :username => 'postgres', :password => node['postgresql']['password']['postgres']})
+  password 'vagrant'
+end
